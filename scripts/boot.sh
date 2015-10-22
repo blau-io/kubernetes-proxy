685ea4b6c2e450b1d897e79c862b62ec191723ad
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-10-22 17:11:58 -0400

    Added confd configuration

---
+++ scripts/boot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eo pipefail
+
+# Loop until Confd created the first config gile
+until /usr/bin/confd -onetime \
+            -node 127.0.0.1:4001 \
+            -config-file /etc/confd/conf.d/nginx.toml
+do sleep 5
+done
+
+# Start Confd
+echo "Starting Confd"
+/usr/bin/confd -interval 10 \
+      -node 127.0.0.1:4001 \
+      -config-file /etc/confd/conf.d/nginx.toml &
+
+# Start Nginx
+echo "Starting Nginx"
+/etc/init.d/nginx start
